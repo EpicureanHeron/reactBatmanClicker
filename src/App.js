aac08f45d1e08ed684d15409e769a64b42c601f3
Joe Thor <joethor1989@gmail.com>
2018-09-15 21:58:51 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import FriendCard from './Components/FriendCard'
+import batman from "./batman.json";
+
+import './App.css';
+
+class App extends React.Component {
+  // Setting the initial state of the Counter component
+  state = {
+    batman
+  };
+  // handleIncrement increments this.state.count by 1
+  removeFriend = id => {
+    // We always use the setState method to update a component's state
+    const batman = this.state.batman.filter(batman => batman.id !== id);
+    this.setState({ batman });
+  };
+
+
+  // The render method returns the JSX that should be rendered
+  render() {
+    return (
+      <div>
+        <h1 className="title">Friends List</h1>
+        {this.state.batman.map(item => <FriendCard
+          key={item.id}
+          removeFriend = {this.removeFriend}
+          id={item.id}
+          name={item.name}
+          image={item.image} />)}
+     </div>
+    );
+
+  }
+
+
+}
+
+export default App;
